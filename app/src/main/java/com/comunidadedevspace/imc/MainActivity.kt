34c23a4ff63ee15511973ae1fd0d274a46457aad
infomiguelito <168372708+infomiguelito@users.noreply.github.com>
2024-08-06 19:27:20 -0300

    TELA INICIAL

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -2,10 +2,31 @@ package com.comunidadedevspace.imc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+// recuperar os componentes
+        // criar variavel e adicionar componente no <edttext>
+
+        val edt_Peso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edt_Altura = findViewById<TextInputEditText>(R.id.edt_altura)
+        val Btncalcular = findViewById<Button>(R.id.btn_calcular)
+
+
+        // componente botão
+
+        Btncalcular.setOnClickListener {
+            val peso = edt_Peso.text
+            val altura = edt_Altura.text
+            println(" miguel ação do botão" + peso)
+            println(" miguel ação do botão" + altura)
+        }
+
+
     }
 }
