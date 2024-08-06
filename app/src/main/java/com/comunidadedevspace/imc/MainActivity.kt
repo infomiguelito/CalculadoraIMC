3ecddd60a4e723835c673e38eec19d22c0b2c811
infomiguelito <168372708+infomiguelito@users.noreply.github.com>
2024-08-06 20:43:41 -0300

    TELA INICIAL

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -13,18 +14,44 @@ class MainActivity : AppCompatActivity() {
 // recuperar os componentes
         // criar variavel e adicionar componente no <edttext>
 
-        val edt_Peso = findViewById<TextInputEditText>(R.id.edt_peso)
-        val edt_Altura = findViewById<TextInputEditText>(R.id.edt_altura)
+        val edt_peso = findViewById<TextInputEditText>(R.id.edt_peso)
+        val edt_altura = findViewById<TextInputEditText>(R.id.edt_altura)
         val Btncalcular = findViewById<Button>(R.id.btn_calcular)
 
 
         // componente botão
 
         Btncalcular.setOnClickListener {
-            val peso = edt_Peso.text
-            val altura = edt_Altura.text
-            println(" miguel ação do botão" + peso)
-            println(" miguel ação do botão" + altura)
+
+            val pesostr: String = edt_peso.text.toString()
+            val alturastr: String = edt_altura.text.toString()
+
+            if (pesostr == "" || alturastr == "") {
+
+                // mostrar msg ao usuario
+
+                Snackbar
+                    .make(
+                    edt_peso,
+                    "Por favor, preencha todos os campos",
+                    Snackbar.LENGTH_LONG
+
+                ) . show()
+
+            } else {
+
+                val peso= pesostr.toFloat()
+                val altura= alturastr.toFloat()
+
+                // calcular imc
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+
+                println(" miguel ação do botão" + resultado)
+            }
+
+
         }
 
 
